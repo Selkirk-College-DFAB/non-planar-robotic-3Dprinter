92a313a6327a364c2c76b2bc25c5bf5e55f21c94
Aras Balali Moghaddam <arasbm@gmail.com>
2024-08-26 14:39:02 -0700

    Update ModbusExtruderControl.ino
    added basic modbus control of extruder speed and position

--- ModbusExtruderControl/ModbusExtruderControl.ino
+++ ModbusExtruderControl/ModbusExtruderControl.ino
@@ -1,9 +1,8 @@
 /*
   Modbus Extruder Control
 
-  Uses ESP8266 to connect to WiFi
-  Read max speed value from a potentiameter connected to A0 and set the speed
-  Read amount of movement from EXTRUDER_REG using modbus over WiFi
+  Uses ESP32 to connect to WiFi
+  Read amount of movement from EXTRUDER_REG and EXTRUDER_SPEED_REG using modbus over WiFi
   Use AccelStepper library to move stepper with max speed to new desired location
 */
 
@@ -19,9 +18,9 @@
 //Modbus Registers Offsets
 const int LED_COIL = 100;
 //Used Pins
-const int LED_PIN = 2; // GPIO2 = D4
-const int STEP_PIN = 5; // GPIO 5 = D1
-const int DIR_PIN = 4;  // GPIO 4 = D2
+const int LED_PIN = 2;
+const int STEP_PIN = 16; 
+const int DIR_PIN = 17;
 const int MAX_SPEED_UPPER = 5000; // upper bound for max speed
 const int MAX_SPEED_LOWER = 500; // lower bound for max speed
 const int STEP_FACTOR = 3;
@@ -31,8 +30,8 @@ const long interval = 400; // Interval to read analog value in milliseconds
 
 //ModbusIP object
 ModbusIP mb;
-const int EXTRUDER_REG = 101; // register for extruder stepper
-
+const int EXTRUDER_REG = 110; // register for extruder stepper
+const int EXTRUDER_SPEED_REG = 111;
 int extruderAccel = 500;
 int extruderSpeed = 1000;
 int extruderGoTo = 0;
@@ -61,37 +60,30 @@ void setup() {
   mb.addCoil(LED_COIL);
 
   mb.addHreg(EXTRUDER_REG);
+  mb.addHreg(EXTRUDER_SPEED_REG);
 
   extruder.setMaxSpeed(extruderSpeed);
   extruder.setAcceleration(extruderAccel);
   extruder.setCurrentPosition(0);
-  extruder.moveTo(10000);
+  extruder.moveTo(20000);
 }
  
 void loop() {
   //Call once inside loop()
   mb.task();
 
-  if (extruderGoTo != mb.Hreg(EXTRUDER_REG)) {
-    extruderGoTo = mb.Hreg(EXTRUDER_REG);
-    //augerSpeed = map(augerVal, 0, 100, 0, 100);
-    Serial.println(extruderGoTo);
-    extruder.moveTo(extruderGoTo * STEP_FACTOR);
+  if (extruderSpeed != mb.Hreg(EXTRUDER_SPEED_REG)){
+    Serial.print("Setting extruder speed: ");
+    extruderSpeed = mb.Hreg(EXTRUDER_SPEED_REG);
+    extruder.setMaxSpeed(extruderSpeed);
+    Serial.println(extruderSpeed);
   }
 
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    // read value of pot and set the speed if it has changed
-    int newSpeed = map(analogRead(A0), 0, 1023, MAX_SPEED_LOWER, MAX_SPEED_UPPER);
-    if (abs(newSpeed - extruderSpeed) > 50) {
-      extruderSpeed = newSpeed;
-      extruder.setMaxSpeed(extruderSpeed);
-      extruder.setAcceleration(extruderSpeed / 2); // TODO: whats the relationship between acceleration and speed?
-
-      Serial.print("New speed set to: ");
-      Serial.println(extruderSpeed);
-    }
+  if (extruderGoTo != mb.Hreg(EXTRUDER_REG)) {
+    Serial.print("Moving extruder: ");
+    extruderGoTo = mb.Hreg(EXTRUDER_REG);
+    extruder.moveTo(extruderGoTo * STEP_FACTOR);
+    Serial.println(extruderGoTo);
   }
 
   extruder.run();
